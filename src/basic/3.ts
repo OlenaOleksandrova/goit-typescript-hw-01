d0ac1fc2be461f6671c2b7b0462302120a0fa3d2
Olena Oleksandrova <ha240886kev@gmail.com>
2024-12-19 01:12:07 +0200

    tasks 1,2,3

---
+++ src/basic/3.ts
@@ -0,0 +1,14 @@
+let mixedType: string | number;
+
+mixedType = 'string'; 
+mixedType = 10;
+
+let state: 'enable' | 'disable';
+
+state = 'enable';
+console.log("State:", state);
+
+state = 'disable';
+console.log("State:", state);
+
+export {};
